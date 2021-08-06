e4727ec773211d5f410c36141544ce7e375fddf4
Peter <griffitp12@gmail.com>
2021-08-06 09:17:31 +0900

    Added some dummy data to play with

---
+++ src/data/DefaultQuestions.js
@@ -0,0 +1,62 @@
+export default DefaultQuestions = [
+  {
+    question: "What do you want to be when you grow up?",
+    birthday: 5,
+    id: 1,
+  },
+  {
+    question: "Who is your best friend?",
+    birthday: 5,
+    id: 2,
+  },
+  {
+    question: "What is something that's scary for you?",
+    birthday: 5,
+    id: 3,
+  },
+  {
+    question: "What do you like to do outside?",
+    birthday: 5,
+    id: 4,
+  },
+  {
+    question: "When is your birthday?",
+    birthday: 5,
+    id: 5,
+  },
+  {
+    question: "Why are you doing this right now?",
+    birthday: 5,
+    id: 6,
+  },
+  {
+    question: "What do you want to be when you grow up?",
+    birthday: 6,
+    id: 7,
+  },
+  {
+    question: "Who is your best friend?",
+    birthday: 6,
+    id: 8,
+  },
+  {
+    question: "What is something that's scary for you?",
+    birthday: 6,
+    id: 9,
+  },
+  {
+    question: "What do you like to do outside?",
+    birthday: 6,
+    id: 10,
+  },
+  {
+    question: "When is your birthday?",
+    birthday: 6,
+    id: 11,
+  },
+  {
+    question: "Why are you doing this right now?",
+    birthday: 6,
+    id: 12,
+  },
+];
---
+++ src/data/DummyUserData.js
@@ -0,0 +1,106 @@
+export default DummyUserData = {
+  users: [
+    {
+      username: "pete",
+      userID: 1,
+      email: "griffitp12@gmail.com",
+      children: [
+        {
+          name: "Mone",
+          pronouns: "M",
+          birthday: "11/30/2019",
+          parentID: 1,
+          childID: 1,
+          capsules: [
+            {
+              capsuleID: 1,
+              capsule_BD: 5,
+              childID: 1,
+              contents: [
+                {
+                  ID: 1,
+                  question: "What do you want to be when you grow up?",
+                  answer: "Fireman",
+                  capsuleID: 1,
+                },
+                {
+                  ID: 2,
+                  question: "Who is your best friend?",
+                  answer: "My brother",
+                  capsuleID: 1,
+                },
+                {
+                  ID: 3,
+                  question: "What is something that's scary for you?",
+                  answer: "Elephants",
+                  capsuleID: 1,
+                },
+                {
+                  ID: 4,
+                  question: "What do you like to do outside?",
+                  answer: "Play and swim and stuff",
+                  capsuleID: 1,
+                },
+                {
+                  ID: 5,
+                  question: "When is your birthday?",
+                  answer: "November 30",
+                  capsuleID: 1,
+                },
+                {
+                  ID: 6,
+                  question: "Why are you doing this right now?",
+                  answer: "Cause Mommy asked me to!",
+                  capsuleID: 1,
+                },
+              ],
+            },
+            {
+              capsuleID: 2,
+              capsule_BD: 6,
+              childID: 1,
+              contents: [
+                {
+                  ID: 1,
+                  question: "What do you want to be when you grow up?",
+                  answer: "An astronaut and a runner",
+                  capsuleID: 2,
+                },
+                {
+                  ID: 2,
+                  question: "Who is your best friend?",
+                  answer: "My brother",
+                  capsuleID: 2,
+                },
+                {
+                  ID: 3,
+                  question: "What is something that's scary for you?",
+                  answer: "Elephants are a little scary. I used to be really scared of them, but now just a little.",
+                  capsuleID: 2,
+                },
+                {
+                  ID: 4,
+                  question: "What do you like to do outside?",
+                  answer: "I looooove to run around!",
+                  capsuleID: 2,
+                },
+                {
+                  ID: 5,
+                  question: "When is your birthday?",
+                  answer: "Today!",
+                  capsuleID: 2,
+                },
+                {
+                  ID: 6,
+                  question: "Why are you doing this right now?",
+                  answer: "For daddy's app",
+                  capsuleID: 2,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
